9bed01e56cf754c934119f01cde5f49b7ee883dd
Anshoswal <anshoswal0006@gmail.com>
2024-12-15 17:19:00 +0530

    Create try.py

---
+++ try.py
@@ -0,0 +1,13 @@
+fl_mdp = {}
+
+for state in range(0, 16):
+
+    # add transitions to other states
+    transitions = {}
+
+    for action in ["Up", "Down", "Right", "Left"]:
+        
+        # do work
+        continue
+
+    fl_mdp[state] = transitions
