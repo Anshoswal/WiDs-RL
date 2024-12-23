9075de492a05eb4818d798407ba3c5f7949cdbf3
Anshoswal <anshoswal0006@gmail.com>
2024-12-23 12:51:34 +0530

    .

--- try.py
+++ try.py
@@ -20,7 +20,8 @@ pi = {
 
 val = dict()
 for state in mdp_transitions:
-    pi[state] = np.random.choice()
+    pi[state] = 0
+    
 def get_new_value_fn(val, mdp, pi, gamma = 1.0):
     new_val = dict()
     state_new_val = 0
@@ -28,13 +29,13 @@ def get_new_value_fn(val, mdp, pi, gamma = 1.0):
         state_new_val = 0
         action = pi[state]
         for my_tupple in mdp[state][action]:
-            prob = my_tupple[0]
-            next_state = my_tupple[1]
-            new_reward = my_tupple[2]
-            done = my_tupple[3]
+            prob,next_state,new_reward,done = my_tupple
+            if next_state == 15:
+                new_reward=1
             state_new_val += prob*(new_reward * (not done) + gamma * val[next_state])
         new_val[state] = state_new_val  
     return new_val
+
 
 #Use to above function to get the new value function, also print how many iterations it took to converge
 def policy_evaluation(val, mdp, pi, epsilon=1e-10, gamma=1.0):
